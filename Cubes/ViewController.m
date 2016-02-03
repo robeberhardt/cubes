081694f1ecba8362b335be8766de96f7ae6c2f45
Rob Eberhardt <rob.eberhardt@gmail.com>
2016-02-03 12:51:37 -0500

    Initial Commit

---
+++ Cubes/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Cubes
+//
+//  Created by Rob Eberhardt on 2/3/16.
+//  Copyright © 2016 Rob Eberhardt. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
